57f161935f3d4f5b00e2c6b1ec1d6816c050d633
Eric <erics1337@gmail.com>
2025-12-04 23:47:03 -0700

    feat: Implement semantic search and centralize embedding storage in shared links, removing the `bookmark_embeddings` table.

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -1,6 +1,6 @@
 console.log('Link Loom background script loaded');
 
 // Listen for bookmark changes
-chrome.bookmarks.onCreated.addListener((id, bookmark) => {
+chrome.bookmarks.onCreated.addListener((_id, bookmark) => {
     console.log('Bookmark created:', bookmark);
 });
